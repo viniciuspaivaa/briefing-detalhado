4a019017aa0f26148cda91b2857c80d7dea4a79e
Vinicius <vinicius.paiva2@outlook.com>
2024-11-06 08:59:13 -0300

    Iniciado desenvolivmento website Vinicius

---
+++ assets/js/global.js
@@ -0,0 +1,5 @@
+const head = document.getElementById('head');
+
+head.innerHTML =
+"<link rel='shortcut icon' href='assets/img/favicon/favicon.ico' type='image/x-icon'>" +
+"<link rel='stylesheet' href='assets/css/global.css'></link>";
