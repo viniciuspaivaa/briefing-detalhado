8b83a3654a103d92ef57336b351b9e42885698ae
Vinicius <vinicius.paiva2@outlook.com>
2024-11-13 11:17:52 -0300

    Corrigida linkagem global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,33 +2,45 @@ const head = document.getElementById('head');
 const body = document.getElementById('body');
 
 head.innerHTML +=
-"<link rel='shortcut icon' href='assets/img/favicon/favicon.ico' type='image/x-icon'>" +
-"<link rel='stylesheet' href='assets/css/global.css'></link>" +
+"<link rel='shortcut icon' href='/assets/img/favicon/favicon.ico' type='image/x-icon'>" +
+"<link rel='stylesheet' href='/assets/css/global.css'>" +
+"<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'>" +
 "<title>BrunaMara</title>";
 
 const header = document.createElement('header');
 header.innerHTML += 
-"<a href='index.html'><img id='logo' src='assets/img/logotipo-recortada.png' alt='logo'></a>" +
+"<a href='/index.html'><img id='logo' src='/assets/img/logotipo-recortada.png' alt='logo'></a>" +
 "<div id='navbar'>" +
     "<div id='menu-flutuante'>" +
         "<a id='roupas-navbar' class='navbar-links' href=''>ROUPAS</a>" +
         "<div id='roupas-box'>" +
-        "<a class='roupas-link' href='index.html#category1-title'>FEMININO</a>" +
-        "<a class='roupas-link' href='index.html#category2-title'>MASCULINO</a>" +
-        "<a class='roupas-link' href='index.html#category3-title'>FEMININO PLUS SIZE</a>" +
-        "<a class='roupas-link' href='index.html#category4-title'>MASCULINO PLUS SIZE</a>" +
+        "<a class='roupas-link' href='/index.html#category1-title'>FEMININO</a>" +
+        "<a class='roupas-link' href='/index.html#category2-title'>MASCULINO</a>" +
+        "<a class='roupas-link' href='/index.html#category3-title'>FEMININO PLUS SIZE</a>" +
+        "<a class='roupas-link' href='/index.html#category4-title'>MASCULINO PLUS SIZE</a>" +
         "</div>" +
     "</div>" +
     "<a class='navbar-links' href=''>CONTATO</a>" +
     "<div id='icons'>" +
-        "<a href='assets/html/login.html'><i class='bi bi-person'></i></a>" +
+        "<a href='/assets/html/login.html'><i class='bi bi-person'></i></a>" +
         "<i class='bi bi-heart'></i>" +
         "<i class='bi bi-heart-fill'></i>" +
         "<i class='bi bi-cart3'></i>" +
     "</div>" +
 "</div>";
-
 body.prepend(header);
+
+const footer = document.createElement('footer');
+footer.innerHTML +=
+"<div id='social-media'>" +
+    "<p>SIGA-NOS NAS REDES</p>" +
+    "<i class='bi bi-instagram'></i>" +
+    "<i class='bi bi-twitter-x'></i>" +
+"</div>" +
+"<div id='footer-content'>" +
+    "<p>TODOS DIREITOS RESERVADOS</p>" +
+"</div>";
+body.appendChild(footer);
 
 const heart = document.getElementsByClassName("bi-heart")[0];
 const heartFill = document.getElementsByClassName("bi-heart-fill")[0];
